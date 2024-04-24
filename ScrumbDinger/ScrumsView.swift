7bf19b674a526c13199d665c51d7025c107322ae
darian <xxy.moming@gmail.com>
2024-04-24 14:19:59 +0800

    feat: add ScrumsView

---
+++ ScrumbDinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  ScrumbDinger
+//
+//  Created by Darian Mitchell  on 2024/4/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
