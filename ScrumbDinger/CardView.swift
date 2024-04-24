c9b42161d83d05096a564b687480782df4ed1073
darian <xxy.moming@gmail.com>
2024-04-24 14:03:23 +0800

    feat: add custom label style

--- ScrumbDinger/CardView.swift
+++ ScrumbDinger/CardView.swift
@@ -19,6 +19,7 @@ struct CardView: View {
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .padding(.trailing, 20)
+                    .labelStyle(.trailingIcon)
             }
             .font(.caption)
         }
---
+++ ScrumbDinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  ScrumbDinger
+//
+//  Created by Darian Mitchell  on 2024/4/24.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
