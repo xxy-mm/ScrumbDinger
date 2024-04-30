7eeb3d16f62224d019c0b057f47c8152cc2aa785
darian <xxy.moming@gmail.com>
2024-04-30 10:20:05 +0800

    feat: add DetailView and Attendee struct

---
+++ ScrumbDinger/DetailView.swift
@@ -0,0 +1,52 @@
+//
+//  DetailView.swift
+//  ScrumbDinger
+//
+//  Created by Darian Mitchell  on 2024/4/30.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundStyle(Color.accentColor)
+                
+                HStack{
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .background(scrum.theme.mainColor)
+                        .foregroundStyle(scrum.theme.accentColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                }
+                .accessibilityElement(children: .combine)
+            }
+            
+            Section("Attendees") {
+                ForEach(scrum.attendees){ attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
