02fa82550ae710f757c1d28f4b3c6ebc3b7dfbc7
darian <xxy.moming@gmail.com>
2024-04-30 15:21:48 +0800

    feat: creating a navigation hierarchy

--- ScrumbDinger/DetailView.swift
+++ ScrumbDinger/DetailView.swift
@@ -13,9 +13,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
                     .foregroundStyle(Color.accentColor)
+                }
                 
                 HStack{
                     Label("Length", systemImage: "clock")
@@ -42,6 +44,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
