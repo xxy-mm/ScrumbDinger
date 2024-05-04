ae8c92876489ebe7962268237d3d9688df9c8dc3
darian <xxy.moming@gmail.com>
2024-05-04 10:58:42 +0800

    feat: add scrumTimer state to meeting view

---
+++ ScrumbDinger/ScrumProgressViewStyle.swift
@@ -0,0 +1,33 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import SwiftUI
+
+struct ScrumProgressViewStyle: ProgressViewStyle {
+    var theme: Theme
+
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(theme.accentColor)
+                .frame(height: 20.0)
+            if #available(iOS 15.0, *) {
+                ProgressView(configuration)
+                    .tint(theme.mainColor)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            } else {
+                ProgressView(configuration)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressView(value: 0.4)
+        .progressViewStyle(ScrumProgressViewStyle(theme: .buttercup))
+        .previewLayout(.sizeThatFits)
+}
