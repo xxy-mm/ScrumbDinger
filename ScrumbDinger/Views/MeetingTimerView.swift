5b7aa5eb45236cd271ef072022c1c52e5ed27acd
darian <xxy.moming@gmail.com>
2024-05-06 11:54:42 +0800

    feat: add MeetingTimerView

--- ScrumbDinger/Views/MeetingTimerView.swift
+++ ScrumbDinger/Views/MeetingTimerView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct MeetingTimerView: View {
     let speakers: [ScrumTimer.Speaker]
     let theme: Theme
-    
+
     private var currentSpeaker: String {
-        speakers.first(where: {!$0.isCompleted})?.name ?? "Someone"
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
     }
+
     var body: some View {
         Circle()
             .strokeBorder(lineWidth: 24)
@@ -26,18 +27,25 @@ struct MeetingTimerView: View {
                 .accessibilityElement(children: .combine)
                 .foregroundColor(theme.accentColor)
             }
+            .overlay {
+                ForEach(speakers) { speaker in
+                    if speaker.isCompleted, let index = speakers.firstIndex(where: {$0.id == speaker.id}) {
+                        SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(theme.mainColor, lineWidth: 12)
+                    }
+                }
+            }
+            .padding(.horizontal)
     }
 }
-
 
 struct MeetingTimerView_Previews: PreviewProvider {
     static var speakers: [ScrumTimer.Speaker] {
         [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
-
     }
-    
+
     static var previews: some View {
         MeetingTimerView(speakers: speakers, theme: .yellow)
     }
 }
-
